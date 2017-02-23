af63ba19920137e5df0cfeb92aaf6f23e8ac1fc7
tuf67096 <tuf67096@TECH-O023.tu.temple.edu>
2017-02-23 14:48:38 -0500

    Test 1

---
+++ src/enigma/Enigma.java
@@ -0,0 +1,21 @@
+
+package enigma;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author tuf67096
+ */
+public class Enigma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        
+        
+        //Dialog Box - basic Input for encryption/Decription and key
+        JOptionPane.showInputDialog("Key");
+    }
+    
+}
