108d9ce0dfd4913922dfe3463259228d6e29d6eb
tuf67096 <tuf67096@TECH-O023.tu.temple.edu>
2017-02-23 15:42:38 -0500

    Tech 2/23

---
+++ src/enigma/Cipher.java
@@ -0,0 +1,30 @@
+
+package enigma;
+//TODO define Nb and Nr both 4
+/**
+ *
+ * @author tuf67096
+ */
+public class Cipher {
+public static final int Nb = 4;
+public static final int Nr = 4;
+    void cipher(byte[] in, byte[] out, byte[] key){
+        //in is the input plain text
+        //out is the output in cipher text
+        //key is the expanded key of size 4*Nb*(Nb+1)
+        byte[][] state = new byte[4][Nb];  ///Nb will always be size 4
+        state = in; 
+        
+        AddRoundKey(state, word, Nb-1);
+        for(int round = 1; round < Nr; round++){
+            SubBytes(state);
+            ShiftRows(state);
+            MixColumns(state);
+            AddRoundKey(state, word, round*Nb, (round+1)*Nb - 1);
+                 
+        }
+        
+        
+    
+    } 
+}
