c3433ef8684f1cbb2d9075a55b88ebca7488b9a3
psteu_000 <psteu_000@10.0.0.143>
2017-03-02 18:34:48 -0500

    3/2

--- src/enigma/Cipher.java
+++ src/enigma/Cipher.java
@@ -5,9 +5,11 @@ package enigma;
  *
  * @author tuf67096
  */
+//This is the model(where work is done) for the MVC model
 public class Cipher {
-public static final int Nb = 4;
-public static final int Nr = 4;
+    public static final int Nb = 4;
+    public static final int Nr = 4;
+    public static final byte[][] sbox  = {{0x16, 0x7c, 0x77}};
     void cipher(byte[] in, byte[] out, byte[] key){
         //in is the input plain text
         //out is the output in cipher text
@@ -27,4 +29,14 @@ public static final int Nr = 4;
         
     
     } 
+    public void SubBytes(byte[][] state){
+        for(int row =0; row < state.length ;row++){ //state.length should be 4
+            for(int col = 0;col[0].length < Nb;col++){ //should be 4 
+                state[row][col] = sbox[state[row][col]];
+            }
+        }
+        
+    }
+    
+    
 }
